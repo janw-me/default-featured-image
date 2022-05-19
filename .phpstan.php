755210e26aae4b071ebb8a2e246f3cf925166b08
Janw Oostendorp <webmaster@janw.me>
2022-05-19 20:42:49 +0200

    Added my default git/ci files.

---
+++ .phpstan.php
@@ -0,0 +1,11 @@
+<?php
+// Config file for PHPstan
+
+/**
+ * plugin constants should always be available, and all are stings.
+ */
+define( 'DFI_VERSION', 'string');
+define( 'DFI_DIR', 'string');
+define( 'DFI_APP_DIR', 'string');
+define( 'DFI_URL', 'string');
+define( 'DFI_NAME', 'string');
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Default featured image.
+
+This is the development repo for [Default featured image](https://wordpress.org/plugins/default-featured-image/).
+Releases done here get auto deployed to wordpress.org.
+
+Feel free to contribute :)
