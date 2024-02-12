c0f42db82f9824a8e5226258f48e80dba5c14c5f
Janw Oostendorp <webmaster@janw.me>
2024-02-12 11:56:09 +0100

    Add support example

---
+++ .support/2024-02-12-random-cat.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Default Featured Image - Random per category
+ * Plugin URI:  https://wordpress.org/support/topic/multiple-images-for-different-categories/
+ * Version:     1.0
+ */
+
+add_filter( 'dfi_thumbnail_id', 'dfi_category_random', 10, 2 );
+function dfi_category_random( $dfi_id, $post_id ) {
+    // Set a different image for posts that have the 'cats' category set.
+    if ( has_category( 'cats', $post_id ) ) {
+        $random_cats = array(
+            7,
+            8,
+        );
+        return $random_cats[ array_rand( $random_cats ) ]
+    }
+    // Set a different image for posts that have the 'dogs' category set.
+    if ( has_category( 'dogs', $post_id ) ) {
+        $random_dogs = array(
+            10,
+            14,
+        );
+        return $random_dogs[ array_rand( $random_dogs ) ]
+    }
+
+    return $dfi_id; // the original featured image id.
+}
